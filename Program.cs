39ef9c2ae643d3645fd11ae49126c441b6db9a50
Pavel <sozonovps@gmail.com>
2023-01-13 23:00:15 +0800

    Написана задача 1

--- Program.cs
+++ Program.cs
@@ -4,14 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//https://github.com/the-aspekt/Lab4.git
+
 namespace Lab4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine();
+            int N;
+            N = Convert.ToInt32(Console.ReadLine());
+            int NSquared = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                NSquared += 2 * i - 1;
+                Console.WriteLine(NSquared);
+            }
+            Console.ReadKey();
         }
     }
 }
