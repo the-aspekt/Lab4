5b6cae4dbbaba03d07d39391544218ccf0dd6e07
Pavel <sozonovps@gmail.com>
2023-01-14 18:58:43 +0800

    написал программы

---
+++ task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;          
+            int i = 0;
+            do
+            {
+                N = Convert.ToInt32(Console.ReadLine());
+                if (N > 0)
+                    i++;
+                else if (N < 0)
+                    i--;
+               
+            } while (N!=0);
+            if (i>0)
+            {
+                Console.WriteLine("Положительных чисел больше на {0}", i);
+            }
+            else if (i==0)
+                Console.WriteLine("Количество положительных и отрицательных чисел равно");
+            else
+            {
+                Console.WriteLine("Отрицательных чисел больше на {0}", Math.Abs(i));
+            }
+             
+            Console.ReadLine();  
+        }
+    }
+}
---
+++ task3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа измеряет, сколько квадратов со стороной С влазит в прямоугольник со сторонами АхВ методом перебора");
+            Console.WriteLine("Введите сторону А прямоугольника АxВ");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите сторону В прямоугольника АxВ");
+            int B = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите сторону С квадрата");
+            int C = Convert.ToInt32(Console.ReadLine());
+            int initialB = B;
+            int initialA = A;
+            int number = 0;
+            while ((A-C) >= 0)
+            {
+                B = initialB;
+                while ((B - C) >= 0)
+                {
+                    B -= C;
+                    number++;
+                }
+                A -= C;
+            }
+            Console.WriteLine("В ваш прямоугольник {2}x{3}влезло {0} квадратов со стороной {1}", number, C, initialA, initialB);
+            Console.ReadLine();
+
+        }
+    }
+}
